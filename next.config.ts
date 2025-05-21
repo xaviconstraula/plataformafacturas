384de021e8af9bbc95cfa1d947d7f0fd5a9a88f1
Nols05 <noelliron@gmail.com>
2025-05-21 17:59:26 +0200

    remove standoalone

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  // output: "standalone",
   serverExternalPackages: ["pdf-to-png-converter", "@napi-rs", "@napi-rs/canvas", "@napi-rs/canvas-win32-x64-msvc"],
   compiler: {
     removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false
